b13e7addd83274802af02c78111924d8e04e61fc
Arthur Cloud <arthurcloud@hotmail.co.uk>
2024-07-08 10:46:10 +0100

    Launcher Update and WG Zombies
    - Adjusted Launcher files to include Win7 recommendation
    - Changed WG on death and extreme death actions to proc zombies when killed via CPM plague knives or PC Plague Sword

---
+++ Test Build/Elite_Dev_playercfg.lua
@@ -0,0 +1,123 @@
+Performance =  
+{
+	Avg_2_0 = 0,
+	Avg_2_1 = 0,
+	Avg_2_2 = 0,
+	Avg_2_3 = 0,
+	Avg_2_4 = 0,
+	Max_2_0 = 0,
+	Max_2_1 = 0,
+	Max_2_2 = 0,
+	Max_2_3 = 0,
+	Max_2_4 = 0,
+}
+chat_options =  
+{
+	join_leave_notifications = false,
+	profanity_filter = true,
+	timestamps = true,
+}
+live =  
+{
+	HeroSelection =  
+	{
+		ordo_malleus =  
+		{
+			hero = 0,
+		},
+		space_marines =  
+		{
+			hero = 0,
+		},
+	},
+	LastUsedMapName = "2p_ashesoftyphon",
+	LastUsedRaceID = 3,
+	LastUsedStartingResources = 1,
+	LastUsedVictoryCondition = 3,
+}
+seen_videos =  
+{
+	num_seen = 4,
+	video_00 = "movies:sega_logo.lua",
+	video_01 = "movies:GW_logo.lua",
+	video_02 = "movies:amd_intro.lua",
+	video_03 = "movies:xp2/xp2_intro.lua",
+}
+teamColours =  
+{
+	lastUsedChapters =  
+	{
+		race_chaos =  
+		{
+			chapterIndex = 0,
+		},
+		race_eldar =  
+		{
+			chapterIndex = 0,
+		},
+		race_imperial_guard =  
+		{
+			chapterIndex = 0,
+		},
+		race_marine =  
+		{
+			chapterIndex = 0,
+		},
+		race_marine1 =  
+		{
+			chapterIndex = 0,
+		},
+		race_necron2 =  
+		{
+			chapterIndex = 0,
+		},
+		race_newrace =  
+		{
+			chapterIndex = 0,
+		},
+		race_ork =  
+		{
+			chapterIndex = 0,
+		},
+		race_tyranid =  
+		{
+			chapterIndex = 0,
+		},
+	},
+	userDefinedColours =  
+	{
+		race_marine =  
+		{
+		},
+		race_marine1 =  
+		{
+		},
+	},
+}
+tips =  
+{
+	annihilate_highest = -1,
+	annihilate_next = 0,
+	chaos_marines_highest = -1,
+	chaos_marines_next = 0,
+	eldar_highest = -1,
+	eldar_next = 0,
+	ffa_solo_highest = -1,
+	ffa_solo_next = 0,
+	ffa_team_highest = -1,
+	ffa_team_next = 0,
+	generic_tips_highest = 0,
+	generic_tips_next = 1,
+	imperial_guard_highest = -1,
+	imperial_guard_next = 0,
+	last_seen_tip_type = "space_marines",
+	orks_highest = -1,
+	orks_next = 0,
+	space_marines_highest = 0,
+	space_marines_next = 1,
+	tyranids_highest = -1,
+	tyranids_next = 0,
+	vptickerwin_annihilate_highest = 0,
+	vptickerwin_annihilate_next = 1,
+}
+
---
+++ Test Build/Elite_playercfg.lua
@@ -0,0 +1,121 @@
+Performance =  
+{
+	Avg_2_0 = 0.00757,
+	Avg_2_1 = 0.00704,
+	Avg_2_2 = 0.00736,
+	Avg_2_3 = 0,
+	Avg_2_4 = 0,
+	Max_2_0 = 0.01009,
+	Max_2_1 = 0.00742,
+	Max_2_2 = 0.00848,
+	Max_2_3 = 0,
+	Max_2_4 = 0,
+}
+chat_options =  
+{
+	join_leave_notifications = false,
+	profanity_filter = true,
+	timestamps = true,
+}
+live =  
+{
+	HeroSelection =  
+	{
+		eldar =  
+		{
+			hero = 0,
+		},
+		ordo_malleus =  
+		{
+			hero = 0,
+		},
+	},
+	LastUsedMapName = "2p_ashesoftyphon",
+	LastUsedRaceID = 3,
+	LastUsedStartingLocation = 1,
+	LastUsedStartingResources = 0,
+	LastUsedVictoryCondition = 3,
+}
+seen_videos =  
+{
+	num_seen = 4,
+	video_00 = "movies:sega_logo.lua",
+	video_01 = "movies:GW_logo.lua",
+	video_02 = "movies:amd_intro.lua",
+	video_03 = "movies:xp2/xp2_intro.lua",
+}
+teamColours =  
+{
+	lastUsedChapters =  
+	{
+		race_chaos =  
+		{
+			chapterIndex = 0,
+		},
+		race_eldar =  
+		{
+			chapterIndex = 0,
+		},
+		race_imperial_guard =  
+		{
+			chapterIndex = 0,
+		},
+		race_marine =  
+		{
+			chapterIndex = 0,
+		},
+		race_marine1 =  
+		{
+			chapterIndex = 0,
+		},
+		race_necron2 =  
+		{
+			chapterIndex = 0,
+		},
+		race_newrace =  
+		{
+			chapterIndex = 0,
+		},
+		race_ork =  
+		{
+			chapterIndex = 0,
+		},
+		race_tyranid =  
+		{
+			chapterIndex = 0,
+		},
+	},
+	userDefinedColours =  
+	{
+		race_marine1 =  
+		{
+		},
+	},
+}
+tips =  
+{
+	annihilate_highest = -1,
+	annihilate_next = 0,
+	chaos_marines_highest = -1,
+	chaos_marines_next = 0,
+	eldar_highest = 0,
+	eldar_next = 1,
+	ffa_solo_highest = -1,
+	ffa_solo_next = 0,
+	ffa_team_highest = -1,
+	ffa_team_next = 0,
+	generic_tips_highest = 1,
+	generic_tips_next = 2,
+	imperial_guard_highest = -1,
+	imperial_guard_next = 0,
+	last_seen_tip_type = "generic_tips",
+	orks_highest = -1,
+	orks_next = 0,
+	space_marines_highest = -1,
+	space_marines_next = 0,
+	tyranids_highest = -1,
+	tyranids_next = 0,
+	vptickerwin_annihilate_highest = -1,
+	vptickerwin_annihilate_next = 0,
+}
+
