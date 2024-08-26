5cc29611000f044e768fdcebb6e05bf9393d887a
nootersnooter <153563035+nootersnooter@users.noreply.github.com>
2024-08-26 18:33:24 +0100

    Game Mode Additions
    Tabletop Mode Implementation
    AI recognition code
    Attrib for some of the upcoming Dark Faith content

---
+++ Test Build/Elite/Data/simulation/scar/winconditions/{}z_tabletop_local.lua
@@ -0,0 +1,20 @@
+Localization = 
+{
+	title			= "Tabletop Mode",
+	win_message 	= "$60100",
+	lose_message 	= "$60200",
+	description		= "$60300"
+}
+
+-- Colours must be specified as a hexadecimal string, without the '0x' prefix
+Config =
+{
+	primaryColour = "D10101",
+	secondaryColour = "D10101"
+}
+
+-- the scenarios that this game type can be played on
+ScenarioFilter = 
+{
+	"default"
+}
