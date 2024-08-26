2afa65bd0fd40dd47c60ca377f91ceadca12b78c
nootersnooter <153563035+nootersnooter@users.noreply.github.com>
2024-08-26 19:25:15 +0100

    adjusted LOC for new VPs

---
+++ Test Build/Elite/Data/simulation/scar/winconditions/{}vpc_mb_local.lua
@@ -0,0 +1,20 @@
+Localization = 
+{
+	title			= "Massive Battles - VPC (750 points)",
+	win_message 	= "$60100",
+	lose_message 	= "$60200",
+	description		= "$60300"
+}
+
+-- Colours must be specified as a hexadecimal string, without the '0x' prefix
+Config =
+{
+	primaryColour = "DC1515",
+	secondaryColour = "FB3131"
+}
+
+-- the scenarios that this game type can be played on
+ScenarioFilter = 
+{
+	"default"
+}
