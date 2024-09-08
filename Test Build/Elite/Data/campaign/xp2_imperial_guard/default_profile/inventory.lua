f635f6610673e9cd3547542427aaf8de7aabe22a
nootersnooter <153563035+nootersnooter@users.noreply.github.com>
2024-09-08 18:11:38 +0100

    Campaign syncs

---
+++ Test Build/Elite/Data/campaign/xp2_imperial_guard/default_profile/inventory.lua
@@ -0,0 +1,273 @@
+inventory =  
+{
+	commander_accessory =  
+	{	
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\accessories\\gk_commander_acc_hereticus",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\accessories\\gk_commander_acc_malleus",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\accessories\\gk_commander_acc_xenos",
+			new = true,
+		},
+	},
+	accessory =  
+	{	
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_modifier_perditio",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_modifier_permadeath",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_modifier_incursion",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_modifier_doppelgangers",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_modifier_alpha_legion",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_modifier_nurgle",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_modifier_arch_enemy",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_modifier_villains",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_imperial_guard\\accessories\\ig_acc_energy_boost",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\accessories\\gk_bitz_l4_epic",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\accessories\\gk_acc_emp_grenade",
+			new = false,
+		},
+	},
+	armour =  
+	{	
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_eversor_l1_common",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_alaric_armour",
+			new = true,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_light_l2_common",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_light_l3_common",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_light_l4_epic",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_light_l5_swiftness",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_power_l1_rare",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_power_l2_warded",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_power_l5_rare",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_power_l7_legendary",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_power_l8_lifesteal",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_arm_corrupt_test",
+			new = false,
+		},
+	},
+	expendable =  
+	{
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_imperial_guard\\accessories\\ig_expendable_experience_booster_1",
+			new = false,
+		},
+	},
+	consumable =  
+	{
+	},
+	weapon =  
+	{
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_daemonhunter_power_sword_l4",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_daemonhunter_power_sword_l5",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_flamer_l3_common",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_hand_flamer_rare_l3",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_crusader_hammer_l3",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_crusader_hammer_l7",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_plasma_gun_l2_common",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_meltagun_l4",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_hellgun_l4_rare",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_hellgun_l5_rare",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_shotgun_l5",
+			new = false,
+		},
+
+
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_shotgun_l3_breaching",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_tactical_set_autogun_l4",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_tactical_set_lascarbine_l6",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_volley_gun_l2_rare",
+			new = false,
+		},
+
+
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_imperial_guard\\weapons_melee\\ig_force_weapon_l5_epic_start",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_imperial_guard\\weapons_melee\\ig_force_weapon_l8_rare_start",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_warding_staff_l2",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_warding_staff_l3",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_warding_staff_l4",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_eversor_blade_l3_common",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_eversor_blade_l6_epic",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_eversor_eviscerator_axe_l4",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_eversor_eviscerator_axe_l5_stunstrike",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_eversor_eviscerator_axe_l9_legendary",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_eversor_gauntlet_l1_common",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_eversor_gauntlet_l5_common",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_crusader_sword_l3",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_crusader_sword_l4",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_crusader_sword_l6",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_nemesis_sword_l3",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_nemesis_halberd_l3",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_nemesis_hammer_l4",
+			new = false,
+		},
+		{
+			blueprint = "wargear\\wargear\\xp2\\campaign\\race_grey_knights\\gk_warding_staff_actual_l4",
+			new = false,
+		},
+	},
+}
+destroyed_inventory =  
+{
+}
+
